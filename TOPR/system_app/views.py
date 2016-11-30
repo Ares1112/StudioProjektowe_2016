7d654f67deda2fc8e0724d17f2d6f75ced4d60d9
Arkadiusz Błasiak <arkadiusz.blasiak@pl.abb.com>
2016-11-30 17:33:12 +0100

    sat

--- TOPR/system_app/views.py
+++ TOPR/system_app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import StrefaZagrozenia
+from .models import StrefaZagrozenia, Szlak
+from satispy import Variable, Cnf
+from satispy.solver import Minisat
 
 @login_required
 def home_view(request):
@@ -12,4 +14,14 @@ def weather_view(request):
 
 @login_required
 def sat_view(request):
-    return render(request, 'sat.html', {'results': 'TEST'})
+    v1 = Variable('v1')
+    v2 = Variable('v2')
+    v3 = Variable('v3')
+    exp = v1 & v2 | v3
+    solver = Minisat()
+    solution = solver.solve(exp)
+    routes = Szlak.objects.all()
+    return_routes = []
+    for route in routes:
+        return_routes.append([route, solution]);
+    return render(request, 'sat.html', {'routes': return_routes})
