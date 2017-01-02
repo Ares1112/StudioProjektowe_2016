8b22bd722f526711aac77867528c356ae1947492
Arkadiusz Błasiak <arkadiusz.blasiak@pl.abb.com>
2017-01-02 16:01:00 +0100

    sat

--- TOPR/system_app/views.py
+++ TOPR/system_app/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import StrefaZagrozenia, Szlak, Pogoda
+from .models import StrefaZagrozenia, Szlak, Pogoda, StanAlarmowy
 from satispy import Variable, Cnf 
 from satispy.solver import Minisat
-from sattable import tableLevel2
+from sattable import tableLevel2,tableLevel3,tableLevel4,tableLevel5
 
 @login_required
 def home_view(request):
@@ -21,11 +21,6 @@ def sat_view(request):
     t1 = Variable('temp1') 
     t2 = Variable('temp2') 
     t3 = Variable('temp3')
-    
-    d1 = Variable('trudnosc1') 
-    d2 = Variable('trudnosc2') 
-    d3 = Variable('trudnosc3')
-    d4 = Variable('trudnosc4')
     
     w1 = Variable('wiatr1') 
     w2 = Variable('wiatr2') 
@@ -47,100 +42,109 @@ def sat_view(request):
     
     solver = Minisat()
     
+    levels = [tableLevel5, tableLevel4, tableLevel3, tableLevel2]
+    
     
     for szlak in routes:
-        RightP = r1 # init of variable
-        Right = r1
-        for val in tableLevel2:
-            if val[0] != szlak.trudnosc:
-                continue
-                
-            if val[1] in (0, 1):
-                RightP = (w1 | w2 | w3)
-            elif val[1] == 2:
-                RightP = (w2 | w3)
-            elif val[1] == 3:
-                RightP = w3
-                
-            if val[2] in (0, 1):
-                RightP &= (m1 | m2 | m3)
-            elif val[2] == 2:
-                RightP &= (m2 | m3)
-            elif val[2] == 3:
-                RightP &= m3
-                
-            if val[3] in (0, 1):
-                RightP &= (t1 | t2 | t3)
-            elif val[3] == 2:
-                RightP &= (t2 | t3)
-            elif val[3] == 3:
-                RightP &= t3
+        levelCounter = 6
+        sw = True
+        for level in levels:
+            if sw:
+                levelCounter-=1
+                RightP = r1 # init of variable
+                Right = r1
+                for val in level:
+                    if val[0] != szlak.trudnosc:
+                        continue
+                        
+                    if val[1] in (0, 1):
+                        RightP = (w1 | w2 | w3)
+                    elif val[1] == 2:
+                        RightP = (w2 | w3)
+                    elif val[1] == 3:
+                        RightP = w3
+                        
+                    if val[2] in (0, 1):
+                        RightP &= (m1 | m2 | m3)
+                    elif val[2] == 2:
+                        RightP &= (m2 | m3)
+                    elif val[2] == 3:
+                        RightP &= m3
+                        
+                    if val[3] in (0, 1):
+                        RightP &= (t1 | t2 | t3)
+                    elif val[3] == 2:
+                        RightP &= (t2 | t3)
+                    elif val[3] == 3:
+                        RightP &= t3
 
-            if val[4] in (0, 1):
-                RightP &= (r1 | r2 | r3)
-            elif val[4] == 2:
-                RightP &= (r2 | r3)
-            elif val[4] == 3:
-                RightP &= r3
+                    if val[4] in (0, 1):
+                        RightP &= (r1 | r2 | r3)
+                    elif val[4] == 2:
+                        RightP &= (r2 | r3)
+                    elif val[4] == 3:
+                        RightP &= r3
+                        
+                    if val[5] in (0, 1):
+                        RightP &= (l1 | l2 | l3 | l4 | l5)
+                    elif val[5] == 2:
+                        RightP &= (l2 | l3 | l4 | l5)
+                    elif val[5] == 3:
+                        RightP &= (l3 | l4 | l5)
+                    elif val[5] == 4:
+                        RightP &= (l4 | l5)
+                    elif val[5] == 5:
+                        RightP &= l5
+                        
+                    Right = RightP
+                        
+                pogoda = Pogoda.objects.filter(szlak__id=szlak.id).order_by('-czas')[0]
+                Actual = d1 # init of variable
                 
-            if val[5] in (0, 1):
-                RightP &= (l1 | l2 | l3 | l4 | l5)
-            elif val[5] == 2:
-                RightP &= (l2 | l3 | l4 | l5)
-            elif val[5] == 3:
-                RightP &= (l3 | l4 | l5)
-            elif val[5] == 4:
-                RightP &= (l4 | l5)
-            elif val[5] == 5:
-                RightP &= l5
-                
-            Right = RightP
-                
-        pogoda = Pogoda.objects.filter(szlak__id=szlak.id).order_by('-czas')[0]
-        Actual = d1 # init of variable
-        
-        if pogoda.deszcz.wiatr.stopien in (0, 1):
-            Actual = w1 & -w2 & -w3
-        elif pogoda.deszcz.wiatr.stopien == 2:
-            Actual = -w1 & w2 & -w3
-        elif pogoda.deszcz.wiatr.stopien == 3:
-            Actual = -w1 & -w2 & w3
-            
-        if pogoda.mgla.stopien in (0, 1):
-            Actual &= m1 & -m2 & -m3
-        elif pogoda.mgla.stopien == 2:
-            Actual &= -m1 & m2 & -m3
-        elif pogoda.mgla.stopien == 3:
-            Actual &= -m1 & -m2 & m3
-            
-        if pogoda.temperatura.stopien in (0, 1):
-            Actual &= t1 & -t2 & -t3
-        elif pogoda.temperatura.stopien == 2:
-            Actual &= -t1 & t2 & -t3
-        elif pogoda.temperatura.stopien == 3:
-            Actual &= -t1 & -t2 & t3
+                if pogoda.deszcz.wiatr.stopien in (0, 1):
+                    Actual = w1 & -w2 & -w3
+                elif pogoda.deszcz.wiatr.stopien == 2:
+                    Actual = -w1 & w2 & -w3
+                elif pogoda.deszcz.wiatr.stopien == 3:
+                    Actual = -w1 & -w2 & w3
+                    
+                if pogoda.mgla.stopien in (0, 1):
+                    Actual &= m1 & -m2 & -m3
+                elif pogoda.mgla.stopien == 2:
+                    Actual &= -m1 & m2 & -m3
+                elif pogoda.mgla.stopien == 3:
+                    Actual &= -m1 & -m2 & m3
+                    
+                if pogoda.temperatura.stopien in (0, 1):
+                    Actual &= t1 & -t2 & -t3
+                elif pogoda.temperatura.stopien == 2:
+                    Actual &= -t1 & t2 & -t3
+                elif pogoda.temperatura.stopien == 3:
+                    Actual &= -t1 & -t2 & t3
 
-        if pogoda.deszcz.stopien in (0, 1):
-            Actual &= r1 & -r2 & -r3
-        elif pogoda.deszcz.stopien == 2:
-            Actual &= -r1 & r2 & -r3
-        elif pogoda.deszcz.stopien == 3:
-            Actual &= -r1 & -r2 & r3
-            
-        if pogoda.lawina.stopien in (0, 1):
-            Actual &= l1 & -l2 & -l3 & -l4 & -l5
-        elif pogoda.lawina.stopien == 2:
-            Actual &= -l1 & l2 & -l3 & -l4 & -l5
-        elif pogoda.lawina.stopien == 3:
-            Actual &= -l1 & -l2 & l3 & -l4 & -l5
-        elif pogoda.lawina.stopien == 4:
-            Actual &= -l1 & -l2 & -l3 & l4 & -l5
-        elif pogoda.lawina.stopien == 5:
-            Actual &= -l1 & -l2 & -l3 & -l4 & l5
-        Alarm = (Actual >> Right) & Actual
-        print Actual, '\n => \n', Right
-        solution = solver.solve(Alarm)
-        print szlak, solution.success
-
+                if pogoda.deszcz.stopien in (0, 1):
+                    Actual &= r1 & -r2 & -r3
+                elif pogoda.deszcz.stopien == 2:
+                    Actual &= -r1 & r2 & -r3
+                elif pogoda.deszcz.stopien == 3:
+                    Actual &= -r1 & -r2 & r3
+                    
+                if pogoda.lawina.stopien in (0, 1):
+                    Actual &= l1 & -l2 & -l3 & -l4 & -l5
+                elif pogoda.lawina.stopien == 2:
+                    Actual &= -l1 & l2 & -l3 & -l4 & -l5
+                elif pogoda.lawina.stopien == 3:
+                    Actual &= -l1 & -l2 & l3 & -l4 & -l5
+                elif pogoda.lawina.stopien == 4:
+                    Actual &= -l1 & -l2 & -l3 & l4 & -l5
+                elif pogoda.lawina.stopien == 5:
+                    Actual &= -l1 & -l2 & -l3 & -l4 & l5
+                Alarm = (Actual >> Right) & Actual
+                solution = solver.solve(Alarm)
+                if solution.success: # if level OK
+                    sw = False
+                    return_routes.append([szlak, StanAlarmowy.objects.filter(poziom=levelCounter)[0]])
+                if levelCounter == 2 and sw: # else level = 1
+                    return_routes.append([szlak, StanAlarmowy.objects.filter(poziom=1)[0]])
             
     return render(request, 'sat.html', {'routes': return_routes})
